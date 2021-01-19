92568b2b13a2d06723fd12149f9fccd19054cd4a
dndgml913 <dndgml913@naver.com>
2021-01-20 03:51:02 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -122,7 +122,9 @@ def is_validated_number(user_input_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-    if is_digit(user_input_number) and is_between_100_and_999(user_input_number):
+    if is_digit(user_input_number) and \
+        is_between_100_and_999(user_input_number) and \
+            not is_duplicated_number(user_input_number):
         result = True
     else:
         result = False
@@ -194,7 +196,7 @@ def get_strikes_or_ball(user_input_number, random_number):
     for i in range(len(user_digits)):
         if user_digits[i] == random_digits[i]:
             strike += 1
-        elif user_digits[i] in set(random_digits)-set(user_digits[i]):
+        elif user_digits[i] in random_digits:
             ball += 1
 
     result = [strike, ball]
